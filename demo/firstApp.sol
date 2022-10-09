89a6aa04356fae90ae7d6479ddfbb6cf3b9a06fd
MasonEast <masoneastc@gmail.com>
2022-10-09 16:21:35 +0800

    first app

---
+++ demo/firstApp.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+  uint public count;
+
+  function get() public view returns (uint) {
+    return count;
+  }
+
+  function inc() public {
+    count += 1;
+  }
+
+  function dec() public {
+    count -= 1;
+  }
+}
